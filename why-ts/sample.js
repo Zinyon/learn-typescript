212c2576c85dd65690ad3d478c2ca20d1d29f04a
Jin Young Park <jy2045@sogang.ac.kr>
2021-02-10 23:43:07 +0900

    [2021/02/10] jsdoc을 이용한 typecheck

---
+++ why-ts/sample.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/**
+ * @param {number} a
+ * @param {number} b
+ */
+
+function sum(a, b) {
+  return a + b;
+}
+
+sum(10, 20);
---
+++ why-ts/sample.ts
@@ -0,0 +1,7 @@
+function sumTS(a: number, b: number): number{
+  return a+b;
+}
+
+const result = sumTS(10,20);
+
+result.toLocaleString();

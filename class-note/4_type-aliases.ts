9605529d9ed4894fa6150880eb544f630fcdf230
Jin Young Park <jy2045@sogang.ac.kr>
2021-02-13 17:33:03 +0900

    [2021/02/13] 타입 별칭

---
+++ class-note/4_type-aliases.ts
@@ -0,0 +1,18 @@
+interface Person {
+  name: string;
+  age: number;
+}
+
+type Person = {
+  name: string;
+  age: number;
+}
+
+var jinyoung: Person = {
+
+}
+
+type MyString = string;
+var str: MyString = 'hello';
+
+type Todo = { id: string; title: string; done: boolean };

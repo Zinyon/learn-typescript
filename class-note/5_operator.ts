81af08e20e006b1707fb185a5486addf0174b5c8
Jin Young Park <jy2045@sogang.ac.kr>
2021-02-13 20:33:14 +0900

    [2021/02/13] 연산자

---
+++ class-note/5_operator.ts
@@ -0,0 +1,30 @@
+function logMessage(value: string | number) {
+  if (typeof value === 'number') {
+    value.toLocaleString();
+  }
+  if (typeof value === 'string') {
+    value.toString();
+  }
+  throw new TypeError('value must be string or number');
+}
+
+interface Developer {
+  name: string;
+  skill: string;
+}
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+function askSomeone(someone: Developer | Person) {
+  someone.name
+  // someone.skill x
+}
+
+function askSomeone(someone: Developer & Person) {
+  someone.name;
+  someone.skill;
+  someone.age;
+}

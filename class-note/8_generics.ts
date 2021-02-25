e454ef003aaa0b7277152c4f0286bb3045fb3c7c
Jin Young Park <jy2045@sogang.ac.kr>
2021-02-26 01:46:31 +0900

    인터페이스에 제네릭 활용

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -6,10 +6,32 @@
 // logText(10);
 // logText('문지열');
 
-// 호출시점에 타입을 넘긴다.
+//호출시점에 타입을 넘긴다.
 function logText<T>(text: T): T {
   console.log(text);
   return text;
 }
 
-logText<string>('하이');
+// 호출할 때 type을 정의
+const a = logText<string>('하이');
+console.log(a);
+a.split('');
+
+const flag = logText<boolean>(true);
+
+// function logText(text: string | number) {
+//   console.log(text);
+//   return text;
+// }
+
+// const a = logText('a');
+// a.split('') // error - 유니온 타입의 문제점
+// logText(10);
+
+interface Dropdown<T> {
+  value: T;
+  selected: boolean;
+}
+
+const obj: Dropdown<number> = { value: 1, selected: true }
+

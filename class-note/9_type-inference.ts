0159c597d6dd76b3c29746b5c5a3403381dc0d5d
Jin Young Park <jy2045@sogang.ac.kr>
2021-02-26 21:28:21 +0900

    type inference

---
+++ class-note/9_type-inference.ts
@@ -0,0 +1,36 @@
+// 타입 추론 기본 1
+var a = 'abc';
+
+function getB(b = 10) {
+  var c = 'hi';
+  return b + c;
+}
+
+// 타입 추론 기본 2
+interface Dropdown<T> {
+  value: T;
+  title: string;
+}
+
+var shoppingItem: Dropdown<string> = {
+  value: 'abc',
+  title: 'hello'
+}
+
+// 타입 추론 기본 3
+interface Drop<T> {
+  value: T;
+  title: string;
+}
+
+interface DetailedDropdown<K> extends Drop<K> {
+  description: string;
+  tag: K;
+}
+
+var detailedItem: DetailedDropdown<string> = {
+  title: 'abc',
+  description: 'ab',
+  value: 'a',
+  tag: 'a'
+}
